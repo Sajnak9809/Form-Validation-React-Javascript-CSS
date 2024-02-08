f04add52847edd7675758b63ffb7b9baf53cbd9d
sajna <sajnak9809.com>
2024-02-08 09:25:58 +0530

    first commit

---
+++ FormValidation/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ValidationInReact from "./validationInReact";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Form Validation</h1>
+      <ValidationInReact />
+    </div>
+  );
+}
